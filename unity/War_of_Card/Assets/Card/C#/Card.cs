59370f6c7ddf06aaa2c5bc3b75f8ba5875545222
YoungHun01 <jdudgnss@naver.com>
2023-11-10 11:33:54 +0900

    .

--- unity/War_of_Card/Assets/Card/C#/Card.cs
+++ unity/War_of_Card/Assets/Card/C#/Card.cs
@@ -6,19 +6,32 @@ using Unity.VisualScripting;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] TMP_Text Health_text;
-    [SerializeField] TMP_Text Damage_text;
-    [SerializeField] TMP_Text Turn_text;
-    [SerializeField] SpriteRenderer Commander_Card;
-    [SerializeField] SpriteRenderer Magic_Card;
-    [SerializeField] SpriteRenderer Place_Card;
-    [SerializeField] SpriteRenderer Unit_Card;
+    [SerializeField] TMP_Text Cost_text;
     [SerializeField] TMP_Text Name_text;
+    [SerializeField] SpriteRenderer image;
     [SerializeField] TMP_Text Description_text;
-    [SerializeField] Sprite cardFront;
-    [SerializeField] Sprite cardBack;
+    [SerializeField] TMP_Text Damage_text;
+    [SerializeField] TMP_Text Health_text;
+    [SerializeField] TMP_Text Turn_text;
+
+    [SerializeField] Sprite Card_Design;
 
     public CardData item;
     bool isFront; //앞 뒤 판단
 
+    public void Setup(CardData item, bool isFront)
+    {
+        this.item = item;
+        this.isFront = isFront;
+        if(this.isFront)
+        {
+            image.sprite=this.item.card_Sprite;
+            Name_text.text=this.item.card_Name;
+            Description_text.text = this.item.card_Description;
+        }
+        else
+        {
+            
+        }
+    }
 }
