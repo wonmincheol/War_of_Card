d26b2c85302c5a9c186eafa5f260aeb61e3a4356
YoungHun01 <jdudgnss@naver.com>
2023-11-10 09:11:46 +0900

    .

--- unity/War_of_Card/Assets/Card/C#/Card.cs
+++ unity/War_of_Card/Assets/Card/C#/Card.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TMP_Text Health_text;
+    [SerializeField] TMP_Text Damage_text;
+    [SerializeField] TMP_Text Turn_text;
+    [SerializeField] SpriteRenderer Commander_Card;
+    [SerializeField] SpriteRenderer Magic_Card;
+    [SerializeField] SpriteRenderer Place_Card;
+    [SerializeField] SpriteRenderer Unit_Card;
+    [SerializeField] TMP_Text Name_text;
+    [SerializeField] TMP_Text Description_text;
+    [SerializeField] Sprite cardFront;
+    [SerializeField] Sprite cardBack;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public CardData item;
+    bool isFront; //앞 뒤 판단
+
 }
--- unity/War_of_Card/Assets/Card/C#/Card_Hand.cs
+++ unity/War_of_Card/Assets/Card/C#/Card_Hand.cs
@@ -4,56 +4,17 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ItemSO : ScriptableObject
-{//카드의 데이터 인덱스 배열화
+public class Dack : ScriptableObject
+{//카드 데이터 배열로 상속
     public CardData[] items;
 }
 public class Card_Hand : MonoBehaviour
 {   
-    public static Card_Hand Inst { get; private set; }
-    void Awake() => Inst = this;
-    [SerializeField] ItemSO itemSO;
+    [SerializeField] Dack dack;
     List<CardData> itemBuffer;
-   /* public GameObject[] cardPrefab; // 카드 프리팹
-    private float cardXOffset = 5.0f; // 각 카드의 X 축 간격
-    private float nextCardX = 0.0f; // 다음 카드의 X 위치
-    private float cardZOffset = 0.5f; // 각 카드의 Z 축 간격
-    private float nextCardZ = 0.0f; // 다음 카드의 Z 위치
-    int draw_count = 0;
-    private void Update()
-    {
-        // 특정 키 (예: 'C' 키)를 눌렀을 때 카드를 생성합니다.
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            if (draw_count <= 5)
-            {
-                SpawnCard();
-                draw_count++;
-            }
-        }
-    }
-    
-    void SpawnCard()
-    {
-        int minR = 0, maxR = 4;
-        int IndexR = Random.Range(minR, maxR);
-        // 새 카드 생성
-        GameObject newCard = Instantiate(cardPrefab[IndexR]);
 
-        // 새 카드의 Y 위치를 조절하여 겹치지 않게 만듭니다.
-        Vector3 cardPosition = new Vector3(nextCardX, 0, nextCardZ);
-        newCard.transform.position = cardPosition;
-
-        // 다음 카드의 X, Z 위치를 업데이트
-        nextCardX += cardXOffset;
-        nextCardZ -= cardZOffset;
-    }*/
    public CardData PopItem()
-    {//카드 사용 후 버퍼에서 삭제
-        if(itemBuffer.Count == 0)
-        {
-            SetupItemBuffer();
-        }
+    {//카드 뽑은 후 버퍼에서 삭제
         CardData item = itemBuffer[0];
         itemBuffer.RemoveAt(0);
         return item;
@@ -62,12 +23,12 @@ public class Card_Hand : MonoBehaviour
     {
         itemBuffer = new List<CardData>();
         //scene 변수 대신 버퍼 제작
-        for(int i=0;i< itemSO.items.Length;i++)
+        for(int i=0;i< dack.items.Length;i++)
         {
-            CardData item = itemSO.items[i];
+            CardData item = dack.items[i];
         }
         for(int i = 0; i < itemBuffer.Count; i++)
-        {
+        {//카드 순서 랜덤
             int rand = Random.Range(i,itemBuffer.Count);
             CardData temp = itemBuffer[i];
             itemBuffer[i] = itemBuffer[rand];
