96a449e409634968459810b2946d54403921257f
choi <sunny03.choi@gmail.com>
2023-11-27 16:13:02 +0900

    페이지를 왔다 갔다 할수 있게 만들고 카드를 선택하면 뒤집히면서 이동할 수있게 만들었습니다.

--- 프밍/ward-of-cards-deak/Assets/script/car.cs
+++ 프밍/ward-of-cards-deak/Assets/script/car.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UIElements;
+
 public class car : MonoBehaviour
 {
     [SerializeField]
@@ -12,7 +16,15 @@ public class car : MonoBehaviour
     private Material backSprite;
     private bool isFlipped=false;
     private bool isFlipping = false;
+    private bool Moving = false;
     public int cardID;
+    private Vector3 orignalPosition;
+    private Vector3 Targetposition;
+    private float cardInterval = 2.0f;
+    void Start()
+    {
+        orignalPosition = transform.position;
+    }
     public void setCardID(int id)
     {
         cardID = id;
@@ -35,10 +47,12 @@ public class car : MonoBehaviour
             if (isFlipped)
             {
                 cardlender.material= AnimalSprite;
+                MoveCardToPosition(orignalPosition);
             }
             else
             {
                 cardlender.material = backSprite;
+                MoveCardToPosition((orignalPosition)+new Vector3(20, 0, 0));
             }
             transform.DOScale(orignalScale, 0.2f).OnComplete(() =>
             {
@@ -47,11 +61,39 @@ public class car : MonoBehaviour
         });
         
     }
+    void MoveCardToPosition(Vector3 destination)
+    {
+        // 현재 카드의 위치에서 목표 위치로 이동
+        Targetposition  =destination;
+
+        // 카드 이동 애니메이션 실행
+        Moving = true;
+    }
     void OnMouseDown()
     {
+        
         if (!isFlipping)
         {
             FlipCard();
         }
     }
+    void FixedUpdate()
+    {
+        // 카드가 이동 중인 경우 지속적으로 해당 좌표로 이동
+        if (Moving)
+        {
+            float step = 10f * Time.fixedDeltaTime; // 이동 속도 조절
+            transform.position = Vector3.MoveTowards(transform.position, Targetposition, step);
+
+            // 목표 위치에 도착하면 이동 종료
+            if (Vector3.Distance(transform.position, Targetposition) < 0.01f)
+            {
+                Moving = false;
+            }
+        }
+    }
 }
+    
+    
+
+    
